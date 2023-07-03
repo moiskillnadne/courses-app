8337db3dc788271e99e0f156b281073318c6dd9d
Riabkov, Viktor <vriabkov@scnsoft.com>
2023-07-03 16:10:09 +0400

    feature: macosbox component

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,3 +1,11 @@
+import { MacosBox } from "~/shared/ui"
+
 export const LandingPage = () => {
-  return <div>Landing page</div>
+  return (
+    <div style={{ backgroundColor: "gray", padding: "16px" }}>
+      <MacosBox>
+        Landing pageLanding pageLanding pageLanding pageLanding pageLanding pageLanding pageLanding pageLanding page
+      </MacosBox>
+    </div>
+  )
 }
