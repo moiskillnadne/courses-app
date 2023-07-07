547feb8c42593771314fee3730ca149490003342
Riabkov, Viktor <vriabkov@scnsoft.com>
2023-07-07 20:30:09 +0400

    fix: course structure data added

--- src/widgets/Landing/WhatCourseContain/CourseStructure.tsx
+++ src/widgets/Landing/WhatCourseContain/CourseStructure.tsx
@@ -10,11 +10,18 @@ export const CourseStructure = () => {
         <div className="course-structure-content">
           <div className={classNames("course-block-item", "course-block-item-default-color", "course-block-item-1")}>
             <div>
-              <h3>Продвинутый HTML / CSS</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde vero autem omnis et recusandae sequi fugit
-                nulla quis iure rerum repudiandae commodi nostrum, ducimus animi optio? Iusto aspernatur ea veniam.
-              </p>
+              <div>
+                <h3>Продвинутый HTML / CSS</h3>
+              </div>
+              <div>
+                <ul>
+                  <li>Структура HTML документа</li>
+                  <li>Тэги</li>
+                  <li>Css</li>
+                  <li>Работа с формами</li>
+                  <li>Адаптивая верстка</li>
+                </ul>
+              </div>
             </div>
           </div>
           <div
@@ -25,11 +32,18 @@ export const CourseStructure = () => {
               "course-block-git-icon",
             )}>
             <div>
-              <h3>GIT & Agile</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde vero autem omnis et recusandae sequi fugit
-                nulla quis iure rerum repudiandae commodi nostrum, ducimus animi optio? Iusto aspernatur ea veniam.
-              </p>
+              <div>
+                <h3>GIT & Agile</h3>
+              </div>
+              <div>
+                <ul>
+                  <li>Установка GIT</li>
+                  <li>Создание локального репозитория</li>
+                  <li>Создание удаленного репозитория</li>
+                  <li>Работа с удаленным репозиторием</li>
+                  <li>Работа с ветками</li>
+                </ul>
+              </div>
             </div>
           </div>
           <div
@@ -40,11 +54,18 @@ export const CourseStructure = () => {
               "course-block-react-icon",
             )}>
             <div>
-              <h3>React</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde vero autem omnis et recusandae sequi fugit
-                nulla quis iure rerum repudiandae commodi nostrum, ducimus animi optio? Iusto aspernatur ea veniam.
-              </p>
+              <div>
+                <h3>React</h3>
+              </div>
+              <div>
+                <ul>
+                  <li>Знакомство с фреймворками</li>
+                  <li>Установка React</li>
+                  <li>Hello world пример</li>
+                  <li>Компоненты, хуки</li>
+                  <li>Жизненый цикл компонентов</li>
+                </ul>
+              </div>
             </div>
           </div>
           <div
@@ -55,11 +76,18 @@ export const CourseStructure = () => {
               "course-block-typescript-icon",
             )}>
             <div>
-              <h3>Typescript</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde vero autem omnis et recusandae sequi fugit
-                nulla quis iure rerum repudiandae commodi nostrum, ducimus animi optio? Iusto aspernatur ea veniam.
-              </p>
+              <div>
+                <h3>Typescript</h3>
+              </div>
+              <div>
+                <ul>
+                  <li>Знакомство с Typescript</li>
+                  <li>Установка Typescript и подключение в проект</li>
+                  <li>Типы и интерфейсы</li>
+                  <li>Дженерики</li>
+                  <li>Enums</li>
+                </ul>
+              </div>
             </div>
           </div>
           <div
@@ -70,20 +98,33 @@ export const CourseStructure = () => {
               "course-block-javascript-icon",
             )}>
             <div>
-              <h3>Javascript</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde vero autem omnis et recusandae sequi fugit
-                nulla quis iure rerum repudiandae commodi nostrum, ducimus animi optio? Iusto aspernatur ea veniam.
-              </p>
+              <div>
+                <h3>Javascript</h3>
+              </div>
+              <div>
+                <ul>
+                  <li>Знакомство с Javascript</li>
+                  <li>Работа с Дом деревом</li>
+                  <li>Типы, переменные, циклы</li>
+                  <li>Event loop</li>
+                  <li>Прототипирование, замыкание</li>
+                </ul>
+              </div>
             </div>
           </div>
           <div className={classNames("course-block-item", "course-block-item-green-color", "course-block-item-6")}>
             <div>
-              <h3>Модуль по трудоустройству</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde vero autem omnis et recusandae sequi fugit
-                nulla quis iure rerum repudiandae commodi nostrum, ducimus animi optio? Iusto aspernatur ea veniam.
-              </p>
+              <div>
+                <h3>Модуль по трудоустройству</h3>
+              </div>
+              <div>
+                <ul>
+                  <li>Структура собеседований</li>
+                  <li>Подготовка резюме</li>
+                  <li>Советы по поиску вакансий</li>
+                  <li>Советы по прохождению собеседований</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
